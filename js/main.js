f3dd045711c13b54ab49df9edd85f123b140ca97
Jeremy Potts <jeremy.potts7@gmail.com>
2021-05-21 14:25:15 -0400

    Added click functionality to button that scrolls back to the top of the page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+//* Check to see if the document has loaded
+if (document.readyState == "loading") {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+  ready()
+}
 
+function ready() {
+  //* Select the scroll button element
+  const scrollToTopButton = document.querySelector('.scrollButton');
+
+  //* Add click functionality to the button element
+  scrollToTopButton.addEventListener('click', scrollToTop);
+
+  //* Define the scroll behavior and destination
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+}
