f5e71173b5af086b64cd82a4944d34f3cbaa235a
Jeremy Potts <jeremy@gearboxbuilt.com>
2022-01-09 16:41:45 -0500

    Added inView animation for tooltip in favor of a simple hover

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
 //* Check to see if the document has loaded
 if (document.readyState == "loading") {
-  document.addEventListener('DOMContentLoaded', ready)
+  document.addEventListener("DOMContentLoaded", ready);
 } else {
-  ready()
+  ready();
 }
 
 function ready() {
   //* Select the scroll button element
-  const scrollToTopButton = document.querySelector('.scrollButton');
+  const scrollToTopButton = document.querySelector(".scrollButton");
 
   //* Add click functionality to the button element
-  scrollToTopButton.addEventListener('click', scrollToTop);
+  scrollToTopButton.addEventListener("click", scrollToTop);
 
   //* Define the scroll behavior and destination
   function scrollToTop() {
     window.scrollTo({
       top: 0,
-      behavior: "smooth"
+      behavior: "smooth",
     });
+  }
+
+  document.addEventListener("scroll", () => {
+    const projects = document.querySelectorAll(".project");
+    projects.forEach((project) => {
+      isInViewport(project);
+    });
+  });
+
+  //* Checks if element is in viewport
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const tooltip = element.firstElementChild;
+    if (rect.top < 300 && rect.top > 0) {
+      return tooltip.classList.add("inView");
+    }
   }
 }
